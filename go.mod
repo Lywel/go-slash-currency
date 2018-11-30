1d3b11f4d37bc3a3ec0128f8d668325eb147cc15
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2018-11-30 06:49:08 +0100

    adding logger config

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module bitbucket.org/ventureslash/go-slash-currency
 require (
 	bitbucket.org/ventureslash/go-ibft v0.0.2
 	github.com/ethereum/go-ethereum v1.8.17
+	github.com/google/logger v0.0.0-20181112113803-324a7c096a0d
 	github.com/gorilla/websocket v1.4.0
 	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"crypto/rand"
-	"log"
-	"os"
-
 	"bitbucket.org/ventureslash/go-ibft/backend"
 	"bitbucket.org/ventureslash/go-slash-currency/currency"
 	"bitbucket.org/ventureslash/go-slash-currency/types"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"flag"
 	eth "github.com/ethereum/go-ethereum/crypto"
+	"github.com/google/logger"
+	"log"
+	"os"
 )
 
 func main() {
+	flag.Parse()
+	logger.SetFlags(log.Lshortfile | log.Lmicroseconds)
 	privkey, err := ecdsa.GenerateKey(eth.S256(), rand.Reader)
 	if err != nil {
 		log.Fatal(err)
